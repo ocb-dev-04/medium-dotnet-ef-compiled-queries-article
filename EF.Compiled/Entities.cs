52d1f44d9d294aa758ba9268a9c4076671757a0f
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-10-29 21:37:58 -0400

    *Add faker and lazy loading to ef core

---
+++ EF.Compiled/Entities.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF.Compiled;
+
+public class Base
+{
+    [Key]
+    public Guid Id { get; set; }
+    public DateTimeOffset Created { get; set; } = DateTimeOffset.UtcNow;
+}
+
+public sealed class User : Base
+{
+    [Required]
+    public string FullName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string ProfileImage { get; set; }
+
+    #region Relations
+
+    public virtual ICollection<Shop> Shops { get; set; } = new HashSet<Shop>();
+
+    #endregion
+}
+
+public sealed class Shop : Base
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string LogoUrl { get; set; }
+
+    #region Relations
+
+    public virtual User Owner { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    public virtual ICollection<Phone> Phones { get; set; } = new HashSet<Phone>();
+    public virtual ICollection<Location> Locations { get; set; } = new HashSet<Location>();
+
+    #endregion
+}
+
+public sealed class Product : Base
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Image { get; set; }
+    [Required]
+    public string Price { get; set; } // "string" just for example, in real app this prop will be an decimal
+
+    #region Relations
+
+    public virtual Shop Shop { get; set; }
+
+    #endregion
+}
+
+public sealed class Phone : Base
+{
+    [Required]
+    public string PhoneNumber { get; set; }
+    [Required]
+    public string AreaCode { get; set; } = "+1";
+    public bool HasWhatsApp { get; set; } = true;
+
+    #region Relations
+
+    public virtual Shop Shop { get; set; }
+
+    #endregion
+}
+
+public sealed class Location : Base
+{
+    [Required]
+    public string Locale { get; set; }
+    public string ShortDescription { get; set; } = string.Empty;
+
+    #region Relations
+
+    public virtual Shop Shop { get; set; }
+
+    #endregion
+}
--- EF.Compiled/Program.cs
+++ EF.Compiled/Program.cs
@@ -2,6 +2,8 @@
 
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Attributes;
+using EF.Compiled;
+using Microsoft.EntityFrameworkCore;
 
 BenchmarkRunner.Run<MainBenchmark>();
 
@@ -10,29 +12,33 @@ public class MainBenchmark
 {
     #region Props
 
-    private IQueryable<Person> _persons;
+    private AppDbContext _context;
 
-    private static readonly Expression<Func<Person, bool>> s_ageExpression = e => e.Age == 23;
-    private static readonly Func<Person, bool> s_ageExpressionCompiled = s_ageExpression.Compile();
+    private static readonly Expression<Func<User, bool>> s_ageExpression = e => e.FullName.Equals("Fulgencio");
+    private static readonly Func<User, bool> s_ageExpressionCompiled = s_ageExpression.Compile();
 
     #endregion
 
     #region Init
 
     [GlobalSetup]
-    public void Init()
+    public async Task Init()
     {
-        _persons = new List<Person> { new("Batman", 81), new("Superman", 9998), new("Catwoman", 23) }
-                    .AsQueryable();
+        DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
+        optionsBuilder.UseInMemoryDatabase(nameof(AppDbContext));
+        optionsBuilder.UseLazyLoadingProxies();
+
+        _context = new AppDbContext(optionsBuilder.Options);
+        var user = await _context.Users.ToListAsync();
+        var shops = await _context.Shops.ToListAsync();
+        Console.WriteLine("All init");
     }
 
     #endregion
 
     [Benchmark]
-    public void Ex() => _persons.Where(s_ageExpression).SingleOrDefault();
+    public void Ex() { }
 
     [Benchmark]
-    public void Ex_Compiled() => _persons.Where(s_ageExpressionCompiled).SingleOrDefault();
+    public void Ex_Compiled() { }
 }
-
-public record class Person(string Name, int Age);
