1d3b3d003d2e55e05c6a090fd9694f9d34b8df27
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-10-30 02:02:00 -0400

    *Finish all

---
+++ EF.Compiled/Initializer.cs
@@ -0,0 +1,90 @@
+﻿using Bogus;
+using Bogus.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.Compiled;
+
+internal class Initializer
+{
+    public static async Task<AppDbContext> Run()
+    {
+        DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
+        optionsBuilder.UseInMemoryDatabase(nameof(AppDbContext));
+        optionsBuilder.UseLazyLoadingProxies();
+
+        AppDbContext _context = new AppDbContext(optionsBuilder.Options);
+        await SeedData(_context);
+
+        return _context;
+    }
+
+    private static async Task SeedData(AppDbContext context)
+    {
+        #region User
+
+        Faker<User> users = new Faker<User>()
+            .RuleFor(x => x.FullName, f => f.Person.FullName)
+            .RuleFor(x => x.Email, f => f.Person.Email)
+            .RuleFor(x => x.ProfileImage, f => f.Image.Image());
+
+        List<User> usersList = users.GenerateBetween(1, 1);
+        await context.Users.AddRangeAsync(usersList);
+        await context.SaveChangesAsync();
+
+        #endregion
+
+        #region Shop
+
+        Faker<Shop> shop = new Faker<Shop>()
+            .RuleFor(x => x.Name, f => f.Person.Company.Name)
+            .RuleFor(x => x.Description, f => f.Person.Company.CatchPhrase)
+            .RuleFor(x => x.LogoUrl, f => f.Image.Image())
+            .RuleFor(x => x.Owner, usersList.First());
+
+        List<Shop> shopList = shop.GenerateBetween(1, 1);
+        await context.Shops.AddRangeAsync(shopList);
+        await context.SaveChangesAsync();
+
+        #endregion
+
+        #region Products
+
+        Faker<Product> product = new Faker<Product>()
+            .RuleFor(x => x.Name, f => f.Commerce.Product())
+            .RuleFor(x => x.Description, f => f.Lorem.Sentence())
+            .RuleFor(x => x.Image, f => f.Image.Image())
+            .RuleFor(x => x.Price, f => f.Commerce.Price(symbol: "$"))
+            .RuleFor(x => x.Shop, shopList.First());
+
+        List<Product> productList = product.GenerateBetween(100, 100);
+        await context.Products.AddRangeAsync(productList);
+        await context.SaveChangesAsync();
+
+        #endregion
+
+        #region Phones
+
+        Faker<Phone> phone = new Faker<Phone>()
+            .RuleFor(x => x.PhoneNumber, f => f.Phone.PhoneNumber())
+            .RuleFor(x => x.Shop, shopList.First());
+
+        List<Phone> phoneList = phone.GenerateBetween(50, 50);
+        await context.Phones.AddRangeAsync(phoneList);
+        await context.SaveChangesAsync();
+
+        #endregion
+
+        #region Location
+
+        Faker<Location> location = new Faker<Location>()
+            .RuleFor(x => x.Locale, f => f.Commerce.Locale)
+            .RuleFor(x => x.ShortDescription, f => f.Address.StreetAddress(true))
+            .RuleFor(x => x.Shop, shopList.First());
+
+        List<Location> locationList = location.GenerateBetween(50, 50);
+        await context.Locations.AddRangeAsync(locationList);
+        await context.SaveChangesAsync();
+
+        #endregion
+    }
+}
--- EF.Compiled/Program.cs
+++ EF.Compiled/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.Linq.Expressions;
 
+using Microsoft.EntityFrameworkCore;
+
+using EF.Compiled;
+
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Attributes;
-using EF.Compiled;
-using Microsoft.EntityFrameworkCore;
 
 BenchmarkRunner.Run<MainBenchmark>();
 
@@ -14,33 +16,54 @@ public class MainBenchmark
 
     private AppDbContext _context;
 
-    private static readonly Expression<Func<User, bool>> s_ageExpression = e => e.FullName.Equals("Fulgencio");
-    private static readonly Func<User, bool> s_ageExpressionCompiled = s_ageExpression.Compile();
+    private static readonly Expression<Func<Shop, bool>> s_NameExpression = e => e.Name.ToLower().Contains("a");
+    private static readonly Func<Shop, bool> s_NameExpressionCompiled = s_NameExpression.Compile();
 
     #endregion
 
     #region Init
 
     [GlobalSetup]
-    public void Init()
+    public async Task Init()
     {
-        DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
-        optionsBuilder.UseInMemoryDatabase(nameof(AppDbContext));
-        optionsBuilder.UseLazyLoadingProxies();
-
-        _context = new AppDbContext(optionsBuilder.Options);
-        
+        _context = await Initializer.Run();
     }
 
     #endregion
 
     [Benchmark]
-    public async Task Ex() {
-        await _context.Shops.Where(w => w.Name.Contains("A")).ToListAsync();
+    public async Task Ex()
+    {
+        await _context.Shops.Where(s_NameExpression).ToListAsync();
     }
 
     [Benchmark]
-    public async Task Ex_Compiled() {
-        await _context.Users.Where(w => w.Email.Contains("@")).ToListAsync();
+    public async Task Ex_Compiled()
+    {
+        _context.Shops.Where(s_NameExpressionCompiled).ToList();
+    }
+
+    [Benchmark]
+    public async Task Ex_SelectFirst()
+    {
+        await _context.Shops.Select(s => new Shop { Name = s.Name }).Where(s_NameExpression).ToListAsync();
+    }
+
+    [Benchmark]
+    public async Task Ex_Compiled_SelectFirst()
+    {
+        _context.Shops.Select(s => new Shop { Name = s.Name }).Where(s_NameExpressionCompiled).ToList();
+    }
+    
+    [Benchmark]
+    public async Task Ex_SelectLast()
+    {
+        await _context.Shops.Where(s_NameExpression).Select(s => new Shop { Name = s.Name }).ToListAsync();
+    }
+
+    [Benchmark]
+    public async Task Ex_Compiled_SelectLast()
+    {
+        _context.Shops.Where(s_NameExpressionCompiled).Select(s => new { Name = s.Name }).ToList();
     }
 }
